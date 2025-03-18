326a160cd45f25d99f3c95a7264ce12e4dc605dc
chekraoui driss <chekraouidriss1@gmail.com>
2025-03-18 03:15:58 +0000

    Routage et sa relation avec controleur

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,6 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+// Routes pour l'authentification
+Route::get('/log_in', [AuthController::class, 'showLoginForm'])->name('login');
+Route::post('/signup', [AuthController::class, 'signup'])->name('signup.submit');
+Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
+Route::get('/shop', [AuthController::class, 'shop'])->middleware('auth')->name('shop');
+
+// Route protégée pour la page shop
+Route::get('/shop', [AuthController::class, 'shop'])->middleware('auth')->name('shop');
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +25,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/buy', function () {
+    return view('buy');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+
+Route::get('/panier', function () {
+    return view('panier');
+});
+Route::get('/shop-single', function () {
+    return view('shop-single');
+});
+
+Route::get('/sign_up', function () {
+    return view('sign_up');
 });
